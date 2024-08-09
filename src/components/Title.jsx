b7548c5b82b17161f7bcf3a81aae58a62d07b608
Louise3Fraser <louise3fraser@gmail.com>
2024-08-08 22:30:54 -0400

    spinning text change

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "../pages-css/MainSections.css";
 import Socials from "./sub_components/Socials";
+import { useTheme, useMediaQuery } from "@mui/material";
 
 export default function Title() {
+    const themeQuery = useTheme();
+  const screenSizeSm = useMediaQuery(themeQuery.breakpoints.up("sm"));
+
+
   return (
     <div className="title">
       <h4 className="h4-title">Louise Fraser</h4>
       <div className="socials-drawer">
-        <div className="spinner-container">
-            hi
+        { screenSizeSm ? (<><div className="spinner-container">
           <svg viewBox="0 0 100 100" className="spinner">
             <path
               id="text-path"
@@ -16,13 +20,14 @@ export default function Title() {
               fill="none"
             />
             <text>
-              <textPath href="#text-path" startOffset="50%" textAnchor="middle">
+              <textPath href="#text-path" >
                 My socials • My socials • My socials •
               </textPath>
             </text>
           </svg>
-        </div>
-        <div style={{ height: "20px" }} />
+        </div> <div style={{ height: "20px" }} /> </>) : (<div/>)}
+        
+        
         <Socials />
       </div>
     </div>
